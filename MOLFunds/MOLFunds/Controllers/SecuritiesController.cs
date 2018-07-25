ac50e8a819d8ad6552219a5fa3894e84c7f805ae
David <David@kjsoft.com.tw>
2018-07-25 14:53:45 +0800

    [Add] Securities

---
+++ MOLFunds/MOLFunds/Controllers/SecuritiesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MOLFunds.Controllers
+{
+    public class SecuritiesController : Controller
+    {
+        // GET: Securities
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SecuritiesMaintain() {
+            return View();
+        }
+    }
+}
