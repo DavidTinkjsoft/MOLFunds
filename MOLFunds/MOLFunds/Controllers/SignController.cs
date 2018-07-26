cedca4837bdc01100e3b9a5f0937a98cb48bacc6
David <David@kjsoft.com.tw>
2018-07-26 12:06:33 +0800

    [Add] Sign Controller

---
+++ MOLFunds/MOLFunds/Controllers/SignController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MOLFunds.Controllers
+{
+    public class SignController : Controller
+    {
+        // GET: Sign
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SignGridView() {
+            return View();
+        }
+    }
+}
