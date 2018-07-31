05e8f27eb8e87f97b7540265b28c37111c22cda6
Lukas <lukas@kjsoft.com.tw>
2018-07-31 16:42:02 +0800

    add 2 views

---
+++ MOLFunds/MOLFunds/Controllers/AddController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MOLFunds.Controllers
+{
+    public class AddController : Controller
+    {
+        // GET: WebCrawler
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult WebCrawler() {
+            return View();
+        }
+
+        public ActionResult OnlineApproval() {
+            return View();
+        }
+    }
+}
